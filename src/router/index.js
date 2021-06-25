62758a5a6a57e4ef6db36838a73f0c7dd4be47c2
Mathieu A. Cormier <63373463+alnumac@users.noreply.github.com>
2021-06-24 21:17:17 -0400

    Fixed favorites

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createWebHistory, createRouter } from "vue-router";
+import { createWebHashHistory, createRouter } from "vue-router";
 import Home from "@/pages/Home.vue";
 import SoundBoard from "@/pages/SoundBoard.vue";
 
@@ -17,7 +17,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory('/vue-soundboard/'),
+  history: createWebHashHistory('/vue-soundboard/'),
   routes
 })
 
